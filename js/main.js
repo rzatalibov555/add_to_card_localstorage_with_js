07e8eb6508419913c9bee7a8a99b660386b543f6
rzatalibov555 <sethubpro@gmail.com>
2023-05-06 16:21:04 +0400

    js localstorage add to card items

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// let username = "3"
+
+
+
+// if(username === "rza" || username == "Rza" || username == "RZa" || username == "RZA"){
+//     console.log("Teacher")
+// }else if(username == 3){
+//     console.log("Js developer")
+// }else if(username == "Aliye"){
+//     console.log("Manager")
+// }else{
+//     console.log("Uppsss")
+// }
+
+
+
+
+
+// let username = prompt("Enter data")
+// username = username.toLowerCase()
+
+// switch(username) {
+//     case "rza":
+//         console.log("Teacher")
+//         break;
+//     case "leman":
+//         console.log("Js developer")
+//         break;
+//     case "aliye":
+//         console.log("Manager")
+//         break;
+//     case "boy":
+//     case "image":
+//         person_boy()
+//         break;
+//     case "girl":
+//     case "anime":
+//         person_girl()
+//         break;
+//     default:
+//         console.log("Uppsss")
+//   }
+
+// function person_boy(){
+//     document.querySelector("body").style.backgroundImage = "url('https://img.freepik.com/free-vector/asian-boy-wears-red-jacket-cartoon-character-sticker_1308-80831.jpg?w=2000')"
+// }
+
+
+// function person_girl(){
+//     document.querySelector("body").style.backgroundImage = "url('https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl_147933-100.jpg?w=2000')"
+// }
+
+
+
+
+// localStorage.setItem("student", "Programming lesson Yehhuu")
+
+// document.querySelector("#name").value = localStorage.getItem("student")
